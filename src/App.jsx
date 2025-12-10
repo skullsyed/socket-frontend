b348a6b3ab739c70dbf2b618a6858195d68b863d
syedaliidc <syed.ali@concertidc.com>
2025-12-10 21:14:27 +0530

    updated file

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,8 @@ export default function App() {
             <AppNavbar />
 
             <Routes>
-              {/* Default route - redirect to dashboard */}
-              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              {/* Default route - redirect to login */}
+              <Route path="/" element={<Navigate to="/login" replace />} />
 
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
